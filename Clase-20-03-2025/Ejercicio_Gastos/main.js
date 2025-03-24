b308c7bc438dcafbb19226011e901de789deddce
ImaFuentes10 <ima_fc@hotmail.com>
2025-03-23 22:27:09 -0700

    feat: :sparkles: Agrego respuesta parcial a ejercicio de control de gastos
    Se resolvió con mis ideas y las respuestas del sensei. Hace falta función de eliminarGastos

--- Clase-20-03-2025/Ejercicio_Gastos/main.js
+++ Clase-20-03-2025/Ejercicio_Gastos/main.js
@@ -1,30 +1,54 @@
 const gastos = [];
 
 function agregarGasto() {
-    const descripcion = document.getElementById("descripcion").value
-    const monto = document.getElementById("monto").value
+    //importo valores ingresados / trim quita espacios al final / parseInt para que sea Int el monto
+    const descripcion = document.getElementById("descripcion").value.trim();
+    const monto = parseFloat(document.getElementById("monto").value.trim());
 
-    if (descripcion = "" || monto === "" || monto < 0){
+    if (descripcion === "" || monto === "" || monto < 0){
         alert("Ingresa un gasto válido")
-    }else{
-        parseInt(monto)
-        const gasto = {descripcion, monto}
-        gastos.push(gasto)
     }
+    //Yo propuse esto
+    /*else{
+        const gasto = {
+            descripcion: descripcion, 
+            monto: monto
+        }
+        gastos.push(gasto)
+    }*/
+    //Pero puede reducirse a esto:
+    gastos.push({descripcion, monto});
+    
     actualizarGastos();
     // 🚀 Completar: Obtener valores del input y validar que sean correctos.
     // 🚀 Completar: Agregar un objeto con descripción y monto al array gastos.
 }
 
 function ordenarGastos() {
-    
+    gastos.sort((a,b) => b.monto - a.monto)
+    actualizarGastos();
     // 🚀 Completar: Ordenar los gastos de mayor a menor.
 }
 
 function calcularTotal() {
+    //con el método reduce sumo todos los montos del array
+    const totalGastado = gastos.reduce((acumulador, gasto) => acumulador + gasto.monto, 0);
+    alert(`Has gastado $${totalGastado} MXN 💰`);
     // 🚀 Completar: Calcular el total gastado con reduce() y mostrarlo en alerta.
 }
 
 function actualizarGastos() {
+    const gastoLista = document.getElementById("lista");
+    //Creo un nuevo elemento 'li' dentro de la 'ul' tomando el gasto dentro del arreglo de gastos
+    
+    gastoLista.innerHTML = gastos.map((gasto,index) =>
+        `<li>${index + 1}. ${gasto.descripcion} - $${gasto.monto}MXN
+        <button onclick="eliminarGasto(${index})" id="eliminar-gasto")>X</button>    
+        </li>`).join("");
     // 🚀 Completar: Renderizar la lista de gastos en el DOM.
+}
+
+//Función para eliminar gastos
+function eliminarGasto(gastosMap) {
+    console.log("prueba");
 }
