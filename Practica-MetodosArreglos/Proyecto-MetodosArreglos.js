c7b78f7b499e9513f81f062da069ac6b77a04c5a
ImaFuentes10 <ima_fc@hotmail.com>
2025-03-30 10:40:52 -0700

    feat: :bulb: Agrego comentarios explicando cada ejemplo

--- Practica-MetodosArreglos/Proyecto-MetodosArreglos.js
+++ Practica-MetodosArreglos/Proyecto-MetodosArreglos.js
@@ -10,43 +10,42 @@ const productos = [
     { nombre: "Celular", precio: 600, categoria: "Electrónica" },
 ];
 
-//filter()
+//filter() - filtro productos menores a 100
 const less$100 = productos.filter(producto => producto.precio < 100);
 console.log(less$100);
 
-//sort()
+//sort() - ordeno por nombre alfabéticamente
 productos.sort((a, b) => a.nombre.localeCompare(b.nombre)); //localeCompare() verifica si va antes o después un string
 console.log(productos);
 
-//map() 
+//map() - creo un nuevo arreglo sólo con los nombres
 const productosNombres = productos.map(producto => producto.nombre);
 console.log(productosNombres);
 
-//find()
+//find() - ordenamos por precio y luego encontramos el primer producto < 0, que será el más barato
 productos.sort((a,b) => a.precio - b.precio);//ordenamos por precio
-console.log(productos);
 const findCheaper = productos.find(producto => producto.precio > 0); //buscamos el primer elemento, osea, el más barato
 console.log(findCheaper);
 
-//forEach()
+//forEach() - aplicamos un 10% de descuento a cada producto
 productos.forEach(producto => {
     const descuento = producto.precio * 0.9;
     console.log(`El producto ${producto.nombre} cuesta $${descuento} por el 10% de descuento`);
 });
 
-//reduce()
+//reduce() - sacamos el costo total de los productos
 const costoTotal = productos.reduce((acum, producto) => acum + producto.precio, 0);
 console.log(costoTotal);
 
-//some()
-const hayRopa = productos.some(producto => producto.nombre === "Zapatos");
+//some() - verificamos si hay ropa en existencia
+const hayRopa = productos.some(producto => producto.categoria === "Ropa");
 console.log(`${
     hayRopa === true ?
     'Sí hay ropa en existencia':
     'No hay ropa en existencia'
 }`);
 
-//every()
+//every() - verificamos si todos los artículos cuestan menos de $500
 const menos500 = productos.every(producto => producto.precio < 500);
 console.log(`${
     menos500 === true ?
@@ -54,7 +53,7 @@ console.log(`${
     'Hay artículos de más de $500'
 }`);
 
-//includes()
+//includes() - verificamos si hay zapatos en existencia
 //utilizo el arreglo productosNombres que cree anteriormente con map() para verificar sólo los nombres del producto
 const hayZapatos = productosNombres.includes("Zapatos");
 console.log(`${
