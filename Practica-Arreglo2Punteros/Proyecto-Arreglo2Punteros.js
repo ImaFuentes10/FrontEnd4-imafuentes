82ec1e6d4c72cc57d1a237c6a2bc6e01de601d4d
ImaFuentes10 <ima_fc@hotmail.com>
2025-03-30 12:53:25 -0700

    feat: :sparkles: Agrego funcionalidad para retornar varias parejas y mostrar a quienes se quedan sin pareja
    Se retornan la mayor cantidad de parejas posibles en orden del arreglo y se muestran las personas que no consiguieron pareja
    
    cambio el parámetro de la función y dentro del código para que funcione con cualquier arreglo y no sólo con 'invitados'

--- Practica-Arreglo2Punteros/Proyecto-Arreglo2Punteros.js
+++ Practica-Arreglo2Punteros/Proyecto-Arreglo2Punteros.js
@@ -2,27 +2,36 @@
 
 const invitados = ["Ana", "Carlos", "Cecilia", "Daniel", "Diana", "Eduardo"];
 
-const encontarPareja = () => {
+const encontarPareja = (arr) => {
     //punteros iniciando enlas primeras dos posiciones del arreglo
     let inicio = 0;
     let siguiente = 1;
 
     //movemos puntero siguiente en cada iteración
-    for (siguiente; siguiente < invitados.length; siguiente++){
+    for (siguiente; siguiente < arr.length; siguiente++){
         
         //comparamos la primera letra de cada elemento con charAt(0)
-        if(invitados[inicio].charAt(0) === invitados[siguiente].charAt(0)){          
-            return console.log(`${invitados[inicio]}, ${invitados[siguiente]}`); //paramos la función y retornamos los nombres
+        if(arr[inicio].charAt(0) === arr[siguiente].charAt(0)){          
+            console.log(`Pareja: ${arr[inicio]}, ${arr[siguiente]}`); //retornamos los nombres
+            arr.splice(inicio, 1); //eliminamos el primer nombre del arreglo
+            arr.splice(siguiente - 1, 1) //eliminamos el segundo nombre del arreglo
+            //reiniciamos punteros, siguiente en 0 porque aumentará 1 en la sig iteración
+            inicio = 0;
+            siguiente = 0;;
         }
 
         //si el segundo puntero llega al final del arreglo...
-        if(siguiente === invitados.length - 1){
+        if(siguiente === arr.length - 1){
             inicio++; //... avanzamos en 1 el primer puntero
             siguiente = inicio; //volvemos al segundo puntero = primero (porque al reiterarse código se le aumentará 1)
         }
-    } 
-    return -1; //detenemos si no encontramos ningún par
+
+        //si el puntero inicio llega al final del arreglo significa que ya se recorrió todo y no hubo parejas
+        if(inicio === arr.length - 1){
+            console.log(`Sin pareja: ${arr.join(', ')}`); //imprimimos las personas faltantes de pareja
+        }
+    }
+    return -1
 }
 
-encontarPareja();
-
+encontarPareja(invitados);
