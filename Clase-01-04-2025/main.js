5e957f0684ddd970f4b76a989e6b6bfc670a86fe
ImaFuentes10 <ima_fc@hotmail.com>
2025-04-02 19:52:02 -0700

    feat: :sparkles: Agrego ejercicios de clase resueltos
    Resueltos con código de senei para recursivo y para 2 punteros
    
    ejercicios resueltos

--- Clase-01-04-2025/main.js
+++ Clase-01-04-2025/main.js
@@ -93,39 +93,70 @@ console.log("Suma par arreglo:",tieneParConSuma(arreglo, 15));
 
 
 
-const str = "ab]cd[ef";
+const corchetes = [
+    "ghugyjgjghj",
+    "ab[c[d]e]f",
+    "ab[c[d]e",
+    "ab[c[d]]e]",
+    "a]b[c",
+    "data[info[level1[level2]]]done",
+    "x[y[z]w]k",
+    "a[b]c"
+];
 
-const corchetesRec = (str) => {
+const comparaRec = (str, index = 0, open = 0) => {
 
+    if (index === str.length) {
+        return open === 0;
+    }
+
+    const char = str[index];
+
+    if (char === '[') {
+        return comparaRec(str, index + 1, open + 1);
+    } else if (char === ']') {
+        if (open === 0) return false; // bracket cerrado sin abrir
+        return comparaRec(str, index + 1, open - 1);
+    } else {
+        return comparaRec(str, index + 1, open);
+    }
 }
 
-const corchetesPunteros = (str) => {
+const comparaPunt = (str) => {
     let left = 0;
     let right = str.length - 1;
-    let suma = 0;
+    let balance = 0;
 
-    while (left <= right){
-        if (str[left] === "[") {
-            left ++;
-            suma ++;
+    while (left <= right) {
+        if (str[left] === '[') {
+            balance++;
+
+            // Buscar cierre correspondiente desde la derecha
+            while (right > left) {
+                if (str[right] === ']') {
+                    balance--;
+                    right--; // Cerramos este ] y lo ignoramos después
+                    break;
+                }
+                right--;
+            }
+
+            // Si no encontramos un cierre adecuado
+            if (balance > 0 && right <= left) return false;
         }
-        if(str[right] === "]"){
-            right --;
-            suma --;
+
+        // Si encontramos un cierre sin apertura previa
+        if (str[left] === ']' && balance <= 0) {
+            return false;
         }
-        /*while (str[left] === "[" && str[right] !== "]"){
-            right --;
-            if (left === right) return false;
-        }
-        while (str[left] !== "[" && str[right] === "]"){
-            left ++;
-            if (left === right) return false;
-        }*/
-       left ++;
-       right --;
+
+        left++;
     }
-    if(suma === 0)return true;
-    if(suma!== 0) return false;
+
+    return balance === 0;
 }
 
-console.log(corchetesPunteros(str));
+
+corchetes.forEach(item => console.log(`${comparaRec(item) ? `Exito al procesar cadena ${item}` : `Error de sintaxis ${item}`}`)); 
+
+corchetes.forEach(item => console.log(`${comparaPunt(item) ? `Exito al procesar cadena ${item}` : `Error de sintaxis ${item}`}`));
