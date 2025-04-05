e6ce42cf939bd2c202c10ad1f7fd5649a9514db3
ImaFuentes10 <ima_fc@hotmail.com>
2025-04-05 14:18:10 -0700

    feat: :sparkles: Agrego problema resuelto
    Agrego resolución con Divide and Conquer (como se solicita) y también con dos punteros y con sliding window
    
    Implementación con dos punteros es más eficiente y con divide and conquer es la menos eficiente

--- Practica-DivideConquer/main.js
+++ Practica-DivideConquer/main.js
@@ -2,5 +2,65 @@
 //Problema: Búsqueda del máximo en un array
 
 // Ejemplo de entrada
-const numbers = [3, 8, 2, 10, 5, 7];
-console.log(findMax(numbers)); // Salida esperada: 10
+const numbers = [3, 8, 2, 10, 5, 7, 4, 6];
+
+//función Divide and Conquer
+function findMax (arr) {
+    //caso base
+    if (arr.length === 1) return arr;
+    //caso arreglo vacío
+    if (arr.length < 1) return 'Ingresa números a comparar';
+    //divisiones del arreglo
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+    //llamadas recursivas
+    const leftMax = findMax(left);
+    const rightMax = findMax(right);
+    //comparativa
+    return Math.max(leftMax, rightMax);
+}
+
+//función dos punteros
+function findMaxDosPunteros (arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left !== right){
+        if (arr[left] < arr[right]) left++;
+        else right --;
+    } return arr[right];
+}
+
+//función Sliding Window
+function findMaxSlidingWindow (arr) {
+    let left = 0;
+    let right = 1;
+    let max = 0;
+    let current;
+
+    while(right < arr.length){
+        current = Math.max(arr[left++], arr[right++]);
+        max = Math.max(current, max);
+    } 
+    return max
+}
+
+//Tiempos de implementación
+//Divide and Conquer
+console.time("Divide and Conquer:");
+console.log(`El número mayor es: ${findMax(numbers)}`);
+console.timeEnd("Divide and Conquer:");
+//Promedio (5): 8.7606ms
+
+//Dos punteros
+console.time("Dos Punteros: ");
+console.log(`El número mayor es: ${findMaxDosPunteros(numbers)}`);
+console.timeEnd("Dos Punteros: ");
+//Promedio (5): 0.1452ms !*!*
+
+//Sliding Window
+console.time("Sliding Window: ");
+console.log(`El número mayor es: ${findMaxSlidingWindow(numbers)}`);
+console.timeEnd("Sliding Window: ");
+//Promedio (5): 0.2102ms
