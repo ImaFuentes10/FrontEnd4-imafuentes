42008003bd324bd68dac99b10268d5e6cdf1dc9c
ImaFuentes10 <ima_fc@hotmail.com>
2025-04-05 13:07:09 -0700

    feat: :sparkles: Agrego archivo Practica Divide and Conquer
    Renombro Carpeta Practica Recursividad

---
+++ Practica-DivideConquer/main.js
@@ -0,0 +1,6 @@
+//PROYECTO DIVIDE AND CONQUER
+//Problema: Búsqueda del máximo en un array
+
+// Ejemplo de entrada
+const numbers = [3, 8, 2, 10, 5, 7];
+console.log(findMax(numbers)); // Salida esperada: 10
