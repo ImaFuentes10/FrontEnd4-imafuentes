d52059264fc6a08adb08f206133b63171144aad9
ImaFuentes10 <ima_fc@hotmail.com>
2025-04-06 12:57:22 -0700

    feat: :sparkles: Completo función eliminarGastos y añado función limpiarCampos
    Ahora se eliminan correctamente las entradas y se reinician los campos al agregar un gasto

--- Clase-20-03-2025/Ejercicio_Gastos/main.js
+++ Clase-20-03-2025/Ejercicio_Gastos/main.js
@@ -18,8 +18,11 @@ function agregarGasto() {
     }*/
     //Pero puede reducirse a esto:
     gastos.push({descripcion, monto});
-    
+
     actualizarGastos();
+
+    limpiarCampos();
+    
     // 🚀 Completar: Obtener valores del input y validar que sean correctos.
     // 🚀 Completar: Agregar un objeto con descripción y monto al array gastos.
 }
@@ -27,6 +30,7 @@ function agregarGasto() {
 function ordenarGastos() {
     gastos.sort((a,b) => b.monto - a.monto)
     actualizarGastos();
+    
     // 🚀 Completar: Ordenar los gastos de mayor a menor.
 }
 
@@ -34,21 +38,30 @@ function calcularTotal() {
     //con el método reduce sumo todos los montos del array
     const totalGastado = gastos.reduce((acumulador, gasto) => acumulador + gasto.monto, 0);
     alert(`Has gastado $${totalGastado} MXN 💰`);
+    
     // 🚀 Completar: Calcular el total gastado con reduce() y mostrarlo en alerta.
 }
 
 function actualizarGastos() {
     const gastoLista = document.getElementById("lista");
-    //Creo un nuevo elemento 'li' dentro de la 'ul' tomando el gasto dentro del arreglo de gastos
     
+    //Creo un nuevo elemento 'li' dentro de la 'ul' tomando el gasto dentro del arreglo de gastos
     gastoLista.innerHTML = gastos.map((gasto,index) =>
         `<li>${index + 1}. ${gasto.descripcion} - $${gasto.monto}MXN
         <button onclick="eliminarGasto(${index})" id="eliminar-gasto")>X</button>    
         </li>`).join("");
+
     // 🚀 Completar: Renderizar la lista de gastos en el DOM.
 }
 
 //Función para eliminar gastos
-function eliminarGasto(gastosMap) {
-    console.log("prueba");
+function eliminarGasto(index) {
+    gastos.splice(index, 1);
+    actualizarGastos();
+}
+
+//Función para limpiar campos
+function limpiarCampos (){
+    document.getElementById("descripcion").value = "";
+    document.getElementById("monto").value = "";
 }
