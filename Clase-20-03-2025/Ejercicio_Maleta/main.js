50bce9d7ccf753ab704591f78ee1c6565961004c
ImaFuentes10 <ima_fc@hotmail.com>
2025-04-06 12:27:27 -0700

    feat: :sparkles: Completo ejercicio 'Maleta'
    Completo todo lo solicitado en el ejercicio 'maleta' y le añado función de eliminar artículo

--- Clase-20-03-2025/Ejercicio_Maleta/main.js
+++ Clase-20-03-2025/Ejercicio_Maleta/main.js
@@ -1,18 +1,57 @@
 const maleta = [];
 
 function agregarItem() {
+    const item = document.getElementById("item").value.trim();
+    if (item === "") {
+        alert("Ingresa un artículo")
+        return
+    }else if(maleta.includes(item)){
+        alert("Artículo ya ingresado")
+        return
+    }
+
+    maleta.push(item);
+
+    actualizarMaleta();
     // 🚀 Completar: Obtener el valor del input y validar que no esté vacío ni duplicado.
     // 🚀 Completar: Agregar el artículo al array maleta y actualizar la lista.
 }
 
 function ordenarMaleta() {
+    maleta.sort();
+    actualizarMaleta();
     // 🚀 Completar: Ordenar los elementos de la maleta alfabéticamente.
 }
 
 function finalizarEmpaque() {
+    if (maleta.length < 3){
+        alert ("Ingresa al menos 3 artículos");
+        return
+    } 
+    
+    alert("Maleta completada");
+
+    for (let i = maleta.length - 1; i >= 0; i--){
+        maleta.pop();
+    }
+    
+    actualizarMaleta();
     // 🚀 Completar: Mostrar alerta si hay menos de 3 artículos, de lo contrario, mensaje de éxito.
 }
 
 function actualizarMaleta() {
+    const lista = document.getElementById("lista");
+
+    lista.innerHTML = maleta.map((item, index) =>
+    `<li>${index + 1}. ${item}
+        <button onclick="eliminarArticulo(${index})" id="eliminar-articulo")>X</button>    
+        </li>`).join("");
     // 🚀 Completar: Renderizar los elementos de la maleta en la lista <ul>.
+}
+
+function eliminarArticulo (index) {
+    const botonEliminar = document.getElementById("eliminar-articulo");
+
+    maleta.splice(index, 1);
+    actualizarMaleta()
 }
